b9afeb1961c9a2b1e695dcf1ce7ef11181d9d905
bilal itani <bilal.itani1234@gmail.com>
2019-01-09 14:39:22 -0500

    Removed absolute path

--- TP1/TP1_Segmentation.py
+++ TP1/TP1_Segmentation.py
@@ -8,8 +8,8 @@ import cv2
 from skimage.measure import compare_ssim
 
 
-basePathInput = "C:\\Users\\bitani\\Desktop\\INF6804\\TP1\\data\\highway\\input"
-basePathGT = "C:\\Users\\bitani\\Desktop\\INF6804\\TP1\\data\\highway\\groundtruth"
+basePathInput = "data/highway/input"
+basePathGT = "data/highway/groundtruth"
 frameNumber = 1700
 
 #Source : https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
@@ -42,21 +42,30 @@ def getFileName(n):
 
 
 def processSegmentationBaselineCars():
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     fgbg = cv2.createBackgroundSubtractorMOG2()
+    min = 1
+    max = -1
     for i in range(1, frameNumber):
-        frame = cv2.imread(basePathInput + "\\in00" + getFileName(i) + ".jpg")
+        frame = cv2.imread(basePathInput + "/in00" + getFileName(i) + ".jpg")
         cv2.imshow("Current frame", frame)
         fgmask = fgbg.apply(frame)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
         cv2.imshow('After Segmentation', fgmask)
+
         if i >= 470:
-            gt = cv2.imread(basePathGT + "\\gt00" + getFileName(i) + ".png", 0)
+            gt = cv2.imread(basePathGT + "/gt00" + getFileName(i) + ".png", 0)
             cv2.imshow("Ground Truth", gt)
-            print("SSIM: {}".format(getSSIM(fgmask, gt)))
+            score = getSSIM(fgmask, gt)
+            if score < min :
+                min = score
+            if score > max :
+                max = score
+            print("SSIM: {}".format(score))
             #print("MSE: {}".format(getMSE(fgmask, gt)))
         #cv2.waitKey(33)
         cv2.waitKey(1)
+    print("Min SSIM : " + str(min) + "\t" + "Max SSIM : " + str(max))
     cv2.destroyAllWindows()
 
 processSegmentationBaselineCars()
