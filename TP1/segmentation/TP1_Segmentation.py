16d4361a2f0dd18ee470bcfc7799ee3c96143de0
bilal itani <bilal.itani1234@gmail.com>
2019-01-22 20:20:30 -0500

    Added IoU for segmentation.

--- TP1/segmentation/TP1_Segmentation.py
+++ TP1/segmentation/TP1_Segmentation.py
@@ -7,14 +7,15 @@ import numpy as np
 import cv2
 from skimage.measure import compare_ssim
 import argparse
+import copy
 
 ap = argparse.ArgumentParser()
 
-ap.add_argument("-p", "--path", required=True,
+ap.add_argument("-p", "--path", default="data/highway/input",
                 help="base path for the images directory")
-ap.add_argument("-f", "--frames", required=True,
+ap.add_argument("-f", "--frames", default="1700",
                 help="frame numbers")
-ap.add_argument("-g", "--groundtruth", required=True,
+ap.add_argument("-g", "--groundtruth", default="data/highway/groundtruth",
                 help="Ground truth path")
 #ap.add_argument("-o", "--output", required=True,
 #                help="Output video location.")
@@ -58,28 +59,68 @@ def getFileName(n):
 def processSegmentationBaselineCars():
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     fgbg = cv2.createBackgroundSubtractorMOG2()
-    min = 1
-    max = -1
+    totalIoU = 0.0
+    numberOfIntersections = 0
+    maxIoU = -1
+    minIoU = 999999999
     for i in range(1, frameNumber):
         frame = cv2.imread(basePathInput + "/in00" + getFileName(i) + ".jpg")
         cv2.imshow("Current frame", frame)
         fgmask = fgbg.apply(frame)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-        cv2.imshow('After Segmentation', fgmask)
-
+        cv2.imshow("After segmentation", fgmask)
+        #Compare rectangles of interest GT v.s method using IoU
         if i >= 470:
+            contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             gt = cv2.imread(basePathGT + "/gt00" + getFileName(i) + ".png", 0)
+            gt_contours, gt_hierarchy = cv2.findContours(gt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            clonedImg = copy.copy(fgmask)
+            for cont in contours:
+                x, y, w, h = cv2.boundingRect(cont)
+                cv2.rectangle(clonedImg, (x, y), (x + w, y + h), (255, 255, 255), 5)
+            cv2.imshow("Region of interest", clonedImg)
+            for cont in gt_contours:
+                x, y, w, h = cv2.boundingRect(cont)
+                cv2.rectangle(gt, (x, y), (x + w, y + h), (255, 255, 255), 5)
             cv2.imshow("Ground Truth", gt)
-            score = getSSIM(fgmask, gt)
-            if score < min :
-                min = score
-            if score > max :
-                max = score
-            print("SSIM: {}".format(score))
-            #print("MSE: {}".format(getMSE(fgmask, gt)))
-        #cv2.waitKey(33)
+            for gt_cont in gt_contours:
+                gt_x, gt_y, gt_w, gt_h = cv2.boundingRect(gt_cont)
+                for cont in contours:
+                    x,y,w,h = cv2.boundingRect(cont)
+                    intersect = intersection((gt_x, gt_y, gt_w, gt_h), (x,y,w,h))
+                    if len(intersect) > 0: #means theres an intersection
+                        un = union((gt_x, gt_y, gt_w, gt_h), (x,y,w,h))
+                        currentIoU = (float(intersect[2]) * float(intersect[3])) / (float(un[2]) * float(un[3]))
+                        totalIoU += currentIoU
+                        numberOfIntersections += 1
+                        if minIoU > currentIoU:
+                            minIoU = currentIoU
+                        if maxIoU < currentIoU:
+                            maxIoU = currentIoU
         cv2.waitKey(1)
-    print("Min SSIM : " + str(min) + "\t" + "Max SSIM : " + str(max))
+    average = totalIoU / float(numberOfIntersections)
+    print("Average : " + str(average))
+    print("Total IoU : " + str(totalIoU))
+    print("total intersections : " + str(numberOfIntersections))
+    print("min IoU :" + str(minIoU))
+    print("max IoU :" + str(maxIoU))
     cv2.destroyAllWindows()
+
+def union(a, b):
+    x = min(a[0], b[0])
+    y = min(a[1], b[1])
+    w = max(a[0] + a[2], b[0] + b[2]) - x
+    h = max(a[1] + a[3], b[1] + b[3]) - y
+    return (x, y, w, h)
+
+def intersection(a, b):
+    x = max(a[0], b[0])
+    y = max(a[1], b[1])
+    w = min(a[0] + a[2], b[0] + b[2]) - x
+    h = min(a[1] + a[3], b[1] + b[3]) - y
+    if w < 0 or h < 0:
+        return ()
+    return (x, y, w, h)
 
 processSegmentationBaselineCars()
