213d71e6a012a46c69b5bf20f6618d7e487d2eb2
bilal itani <bilal.itani1234@gmail.com>
2019-02-16 12:27:11 -0500

    Fixed the code, added more interesting stats. Ready for shipping.

--- TP2/tracking.py
+++ TP2/tracking.py
@@ -37,7 +37,11 @@ def readGroundTruth():
 def getGroundTruthRectangle(frameNumber):
     global gtFile
     data = gtFile[frameNumber].split(',')
-    return int(float(data[0])), int(float(data[1])), int(float(data[2])), int(float(data[3]))
+    if len(data) > 4:
+        return int(float(data[2])), int(float(data[3])), int(float(data[6]) - float(data[2])), int(
+            float(data[7]) - float(data[3]))
+    else:
+        return int(float(data[0])), int(float(data[1])), int(float(data[2])), int(float(data[3]))
 
 def detect_faces_LBP(f_cascade, colored_img, scaleFactor=1.1):
     # just making a copy of image passed, so that passed image is not changed
@@ -49,17 +53,17 @@ def detect_faces_LBP(f_cascade, colored_img, scaleFactor=1.1):
     # let's detect multiscale (some images may be closer to camera than others) images
     faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5);
 
-    rectX = None
-    rectY = None
-    rectW = None
-    rectH = None
+    rectX = []
+    rectY = []
+    rectW = []
+    rectH = []
     # go over list of faces and draw them as rectangles on original colored img
     for (x, y, w, h) in faces:
         cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        rectX = x
-        rectY = y
-        rectW = w
-        rectH = h
+        rectX.append(x)
+        rectY.append(y)
+        rectW.append(w)
+        rectH.append(h)
     return img_copy, rectX, rectY, rectW, rectH
 
 
@@ -86,7 +90,7 @@ def detectFaceDlibHog(detector, frame, inHeight=1000, inWidth=0):
                   int(faceRect.bottom()*scaleHeight) - int(faceRect.top()*scaleHeight)]
         bboxes.append(cvRect)
         cv2.rectangle(frameDlibHog, (cvRect[0], cvRect[1]), (cvRect[2] + cvRect[0], cvRect[3] + cvRect[1]), (0, 255, 0), int(round(frameHeight/150)), 4)
-        print(str(cvRect[0]) + "\t" + str(cvRect[1]) + "\t" + str(cvRect[2]) + "\t" + str(cvRect[3]))
+        #print(str(cvRect[0]) + "\t" + str(cvRect[1]) + "\t" + str(cvRect[2]) + "\t" + str(cvRect[3]))
     return frameDlibHog, bboxes
 
 
@@ -97,12 +101,13 @@ def processHOG():
     maxIoU = -1
     minIoU = 999999999
     totalDetect = 0
-    sortedIoU = []
+    sortedIoUAllFrames = []
+    sortedIoUWhenDetected = []
     for i in range(1, int(args["frames"])):
         frame = cv2.imread(args["path"] + "/" + getFileName(i) + ".jpg")
         outDlibHog, bboxes = detectFaceDlibHog(hogFaceDetector, frame)
+        x_gt, y_gt, gt_width, gt_height = getGroundTruthRectangle(i - 1)
         if len(bboxes) > 0 :
-            x_gt, y_gt, gt_width, gt_height = getGroundTruthRectangle(i - 1)
             intersect = intersection((x_gt, y_gt, gt_width, gt_height), (bboxes[0][0], bboxes[0][1], bboxes[0][2], bboxes[0][3]))
             if len(intersect) > 0:
                 un = union((x_gt, y_gt, gt_width, gt_height), (bboxes[0][0], bboxes[0][1], bboxes[0][2], bboxes[0][3]))
@@ -112,23 +117,38 @@ def processHOG():
                 if maxIoU < currentIoU:
                     maxIoU = currentIoU
                 totalIoU += currentIoU
-                sortedIoU.append(currentIoU)
+                sortedIoUAllFrames.append(currentIoU)
+                sortedIoUWhenDetected.append(currentIoU)
+                totalDetect += 1
         else:
-            sortedIoU.append(0.0)
-        totalDetect += 1
-        cv2.imshow("DLIB HOG", outDlibHog)
+            sortedIoUAllFrames.append(0.0)
+        #totalDetect += 1
+        cv2.rectangle(outDlibHog, (x_gt, y_gt), (x_gt + gt_width, y_gt + gt_height), (255, 0, 0),
+                      4)  # blue : ground truth
+        cv2.imshow("HOG", outDlibHog)
         cv2.waitKey(10)
-    average = totalIoU / float(totalDetect)
-    median = -1
-    sortedIoU.sort()
-    if len(sortedIoU) % 2 == 0:
-        median = (sortedIoU[int(len(sortedIoU) / 2)] + sortedIoU[int(len(sortedIoU) / 2) - 1]) / 2.0
+    averageAllFrames = totalIoU / float(int(args["frames"]))
+    averageDetectedFrames = totalIoU / float(totalDetect)
+    medianAllFrames = -1
+    medianDetectedFrames = -1
+    sortedIoUAllFrames.sort()
+    sortedIoUWhenDetected.sort()
+    if len(sortedIoUAllFrames) % 2 == 0:
+        medianAllFrames = (sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2)] + sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2) - 1]) / 2.0
     else:
-        median = sortedIoU[int(len(sortedIoU) / 2)]
-    print("Median : " + str(median))
-    print("Average : " + str(average))
+        medianAllFrames = sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2)]
+
+    if len(sortedIoUWhenDetected) % 2 == 0:
+        medianDetectedFrames = (sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2)] + sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2) - 1]) / 2.0
+    else:
+        medianDetectedFrames = sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2)]
+    print("Median IoU all frames: " + str(medianAllFrames))
+    print("Median IoU deteted frames: " + str(medianDetectedFrames))
+    print("Average IoU all frames : " + str(averageAllFrames))
+    print("Average IoU detected frames : " + str(averageDetectedFrames))
     print("Total IoU : " + str(totalIoU))
-    print("total intersections : " + str(totalDetect))
+    print("total detections : " + str(totalDetect))
+    print("total frames : " + args["frames"])
     print("min IoU :" + str(minIoU))
     print("max IoU :" + str(maxIoU))
 #From : https://github.com/informramiz/Face-Detection-OpenCV
@@ -140,39 +160,56 @@ def processLBP():
     maxIoU = -1
     minIoU = 999999999
     totalDetect = 0
-    sortedIoU = []
+    sortedIoUAllFrames = []
+    sortedIoUWhenDetected = []
     for i in range(1, int(args["frames"])):
         frame = cv2.imread(args["path"] + "/" + getFileName(i) + ".jpg")
         faces_detected_img, rectX, rectY, rectW, rectH = detect_faces_LBP(lbp_face_cascade, frame)
+        x_gt, y_gt, gt_width, gt_height = getGroundTruthRectangle(i - 1)
         #print(str(rectX))
-        if rectX is not None and rectY is not None and rectW is not None and rectH is not None:
-            x_gt, y_gt, gt_width, gt_height = getGroundTruthRectangle(i - 1)
-            intersect = intersection((x_gt, y_gt, gt_width, gt_height), (rectX, rectY, rectW, rectH))
-            if len(intersect) > 0 : #theres an intersection
-                un = union((x_gt, y_gt, gt_width, gt_height), (rectX, rectY, rectW, rectH))
-                currentIoU = (float(intersect[2]) * float(intersect[3])) / (float(un[2]) * float(un[3]))
-                if minIoU > currentIoU:
-                    minIoU = currentIoU
-                if maxIoU < currentIoU:
-                    maxIoU = currentIoU
-                totalIoU += currentIoU
-                sortedIoU.append(currentIoU)
+        if len(rectX) > 0 :
+            for j in range(0, len(rectX)):
+                intersect = intersection((x_gt, y_gt, gt_width, gt_height), (rectX[j], rectY[j], rectW[j], rectH[j]))
+                if len(intersect) > 0: #theres an intersection
+                    un = union((x_gt, y_gt, gt_width, gt_height), (rectX[j], rectY[j], rectW[j], rectH[j]))
+                    currentIoU = (float(intersect[2]) * float(intersect[3])) / (float(un[2]) * float(un[3]))
+                    if minIoU > currentIoU:
+                        minIoU = currentIoU
+                    if maxIoU < currentIoU:
+                        maxIoU = currentIoU
+                    totalIoU += currentIoU
+                    sortedIoUAllFrames.append(currentIoU)
+                    sortedIoUWhenDetected.append(currentIoU)
+                    totalDetect += 1
+                    break
         else:
-            sortedIoU.append(0.0)
-        totalDetect += 1
-        cv2.imshow("LBP_Experienced", faces_detected_img)
+            sortedIoUAllFrames.append(0.0)
+        cv2.rectangle(faces_detected_img, (x_gt, y_gt), (x_gt + gt_width, y_gt + gt_height), (255, 0, 0),
+                      4)  # blue : ground truth
+        cv2.imshow("LBP", faces_detected_img)
         cv2.waitKey(10)
-    average = totalIoU / float(totalDetect)
-    median = -1
-    sortedIoU.sort()
-    if len(sortedIoU) % 2 == 0:
-        median = (sortedIoU[int(len(sortedIoU) / 2)] + sortedIoU[int(len(sortedIoU) / 2) - 1]) / 2.0
+    averageAllFrames = totalIoU / float(int(args["frames"]))
+    averageDetectedFrames = totalIoU / float(totalDetect)
+    medianAllFrames = -1
+    medianDetectedFrames = -1
+    sortedIoUAllFrames.sort()
+    sortedIoUWhenDetected.sort()
+    if len(sortedIoUAllFrames) % 2 == 0:
+        medianAllFrames = (sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2)] + sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2) - 1]) / 2.0
     else:
-        median = sortedIoU[int(len(sortedIoU) / 2)]
-    print("Median : " + str(median))
-    print("Average : " + str(average))
+        medianAllFrames = sortedIoUAllFrames[int(len(sortedIoUAllFrames) / 2)]
+
+    if len(sortedIoUWhenDetected) % 2 == 0:
+        medianDetectedFrames = (sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2)] + sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2) - 1]) / 2.0
+    else:
+        medianDetectedFrames = sortedIoUWhenDetected[int(len(sortedIoUWhenDetected) / 2)]
+    print("Median IoU all frames: " + str(medianAllFrames))
+    print("Median IoU deteted frames: " + str(medianDetectedFrames))
+    print("Average IoU all frames : " + str(averageAllFrames))
+    print("Average IoU detected frames : " + str(averageDetectedFrames))
     print("Total IoU : " + str(totalIoU))
-    print("total intersections : " + str(totalDetect))
+    print("total detections : " + str(totalDetect))
+    print("total frames : " + args["frames"])
     print("min IoU :" + str(minIoU))
     print("max IoU :" + str(maxIoU))
 
